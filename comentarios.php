e5482f6ee59452e119d594ae9ab80f0333ca5f93
SeDeX <Paulojorgelemosilva@gmail.com>
2021-11-01 21:39:04 -0300

    Modificações totais

--- comentarios.php
+++ comentarios.php
@@ -7,11 +7,11 @@ $comentario = $_POST['comentarios'];
 echo "<p>Nome: $nome </p>";
 echo "<p>Comentarios: $comentario </p>";
 
-$sql = "insert into comentarios (nome, comentario) values ('$nome', '$comentario');";
+$sql = "insert into comentarios (nome, turma, comentario) values ('$nome','$turma', '$comentario');";
 
 echo $sql;
 
-$conexao = new PDO('mysql:host=127.0.0.1;dbname=site','root','');
+$conexao = new PDO('mysql:host=127.0.0.1;dbname=site','Paulo','ktYuSocfQo4c19lo');
 $conexao->exec($sql);
 
 echo "<h2>Comentarios gravado com sucesso</h2>";
